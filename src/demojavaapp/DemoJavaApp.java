813b2d863e0c6df4cff98320a8df17aa55139e3a
MJBoss <bustamantemichaeljohn@gmail.com>
2024-08-21 11:40:07 +0800

    first repo

---
+++ src/demojavaapp/DemoJavaApp.java
@@ -0,0 +1,16 @@
+
+package demojavaapp;
+
+
+public class DemoJavaApp {
+
+    public static void main(String[] args) {
+        
+      
+            Grades gr = new Grades();
+            gr.getName("Mike", 10111, 2.5, 1.8, 2.1, 2.1);
+            gr.displayGrade();
+       
+    }
+    
+}
